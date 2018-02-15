42b6054836e6f122e68f2aa8b6db7df213fb000c
Theo McCaie <tam203@gmail.com>
2018-02-15 11:29:32 +0000

    show xkcd command on panel

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import {
   JupyterLab, JupyterLabPlugin
 } from '@jupyterlab/application';
 
+import {ICommandPalette} from '@jupyterlab/apputils';
+import {Widget} from '@phosphor/widgets';
+
 import '../style/index.css';
 
 
@@ -11,8 +14,27 @@ import '../style/index.css';
 const extension: JupyterLabPlugin<void> = {
   id: 'xkcd_ext',
   autoStart: true,
-  activate: (app: JupyterLab) => {
+  requires: [ICommandPalette],
+  activate: (app: JupyterLab, palette:ICommandPalette) => {
     console.log('JupyterLab extension xkcd_ext is activated!');
+    let widget:Widget = new Widget();
+    widget.id = 'xkcd-jupyterlab';
+    widget.title.label = 'xkcd.com';
+    widget.title.closable = true;
+
+    const command:string = 'xkcd:open';
+    app.commands.addCommand(command, {
+      label: 'Random xkcd commic',
+      execute: () => {
+        if(!widget.isAttached){
+          app.shell.addToMainArea(widget);
+        }
+        app.shell.activateById(widget.id);
+      }
+    })
+
+    palette.addItem({command:command, category:"Tutorial"});
+    
   }
 };
 
